ff07ba4df698fd317fc00edcc548a6ddafe4edb2
stephen hernandez <hrndz.stphn@gmail.com>
2026-02-11 10:33:49 -0500

    Delete plotting directory

--- plotting/plotParticleMotion.m
+++
@@ -1,8 +0,0 @@
-function plotParticleMotion(S,cmp1,cmp2,ax)
-if nargin < 4; ax = newplot; end
-if nargin < 2; cmp1 = 1; end
-if nargin < 3; cmp2 = 2; end
-
-e = S(cmp1).d;
-n = S(cmp2).d;
-plot(ax,e,n);
