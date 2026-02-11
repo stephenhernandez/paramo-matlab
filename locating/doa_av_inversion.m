8cf3096ab2482f0a6b02ae44d3d5cc02cd8f0cb7
stephen hernandez <hrndz.stphn@gmail.com>
2026-02-11 10:50:23 -0500

    Delete locating directory

--- locating/doa_av_inversion.m
+++
@@ -1,7 +0,0 @@
-function [doa,horvel] = doa_av_inversion(dT,Ginv)
-%[doa,horvel,bel] = doa_av_inversion(dT,G)
-slow = Ginv*dT;
-doa = 90-atan2d(slow(2),slow(1))+180;
-horSlow = rssq(slow(1:2));
-horvel = 1./horSlow;
-%bel = atan2d(slow(3),horSlow);
