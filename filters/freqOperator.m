358b9aceea499b0885286d67a1a07100263a7821
stephen hernandez <hrndz.stphn@gmail.com>
2026-02-11 10:41:56 -0500

    Delete filters directory

--- filters/freqOperator.m
+++
@@ -1,7 +0,0 @@
-function [Hbu,Fbu] = freqOperator(npts,lfc,hfc,Fs,npoles)
-if nargin < 5; npoles = 4; end
-if nargin < 4;  Fs = 1; end
-
-%%
-Hd = zpkOperator(lfc,hfc,Fs,npoles);        %butterworth filter
-[Hbu,Fbu] = freqz(Hd,npts,"whole",Fs);
